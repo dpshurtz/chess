04d4b29b48ab4c5a9e9b961ae4091a8f8dea76a0
Chiberty <112521861+Chiberty@users.noreply.github.com>
2026-01-18 16:40:07 -0700

    Implements ChessMove class

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -12,6 +12,7 @@ public class ChessPiece {
 
     private final ChessGame.TeamColor pieceColor;
     private final ChessPiece.PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
