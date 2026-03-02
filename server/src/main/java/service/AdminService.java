33b16f2c6568e76548fe7135e50a6ab1e94bc0d3
Chiberty <112521861+Chiberty@users.noreply.github.com>
2026-03-02 07:50:07 -0700

    UserService implemented

--- server/src/main/java/service/AdminService.java
+++ server/src/main/java/service/AdminService.java
@@ -4,6 +4,8 @@ import dataaccess.AuthDAO;
 import dataaccess.GameDAO;
 import dataaccess.UserDAO;
 
+import java.util.UUID;
+
 public class AdminService {
     private final AuthDAO authDAO;
     private final GameDAO gameDAO;
@@ -19,5 +21,9 @@ public class AdminService {
         authDAO.clear();
         gameDAO.clear();
         userDAO.clear();
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
     }
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,8 +1,15 @@
 package service;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
+import io.javalin.http.ForbiddenResponse;
+import io.javalin.http.UnauthorizedResponse;
+import model.AuthData;
+import model.UserData;
 import serviceobjects.*;
+
+import java.util.Objects;
 
 public class UserService {
     private final AuthDAO authDAO;
@@ -13,15 +20,41 @@ public class UserService {
         this.userDAO = userDAO;
     }
 
-    public RegisterResult register(RegisterRequest registerRequest) {
-        return null;
+    public RegisterResult register(RegisterRequest registerRequest)
+            throws ForbiddenResponse, DataAccessException {
+        UserData user = userDAO.getUser(registerRequest.username());
+        if (user != null) {
+            throw new ForbiddenResponse("already taken");
+        }
+        userDAO.createUser(new UserData(
+                registerRequest.username(),
+                registerRequest.password(),
+                registerRequest.email()));
+
+        String authToken = AdminService.generateToken();
+        authDAO.createAuth(new AuthData(authToken, registerRequest.username()));
+
+        return new RegisterResult(registerRequest.username(), authToken);
     }
 
-    public LoginResult login(LoginRequest loginRequest) {
-        return null;
+    public LoginResult login(LoginRequest loginRequest)
+            throws UnauthorizedResponse, DataAccessException {
+        UserData user = userDAO.getUser(loginRequest.username());
+        if (user == null) {
+            throw new UnauthorizedResponse("Username not found");
+        }
+        else if (!Objects.equals(user.password(), loginRequest.password())) {
+            throw new UnauthorizedResponse("Incorrect password");
+        }
+
+        String authToken = AdminService.generateToken();
+        authDAO.createAuth(new AuthData(authToken, loginRequest.username()));
+
+        return new LoginResult(loginRequest.username(), authToken);
     }
 
-    public void logout(LogoutRequest logoutRequest) {
-
+    public void logout(LogoutRequest logoutRequest)
+            throws DataAccessException {
+        authDAO.deleteAuth(logoutRequest.authToken());
     }
 }
