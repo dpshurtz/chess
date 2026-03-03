44e4445c01e88b3179444d559f7a9675651249fe
Chiberty <112521861+Chiberty@users.noreply.github.com>
2026-03-02 20:00:29 -0700

    Some comments

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,16 +11,20 @@ import service.AdminService;
 import service.GameService;
 import service.UserService;
 
+/**
+ * Creates a chess server using javalin
+ */
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
-
+        // Initialize DAOs
         AuthDAO authDAO = new MemoryAuthDAO();
         GameDAO gameDAO = new MemoryGameDAO();
         UserDAO userDAO = new MemoryUserDAO();
 
+        // Initialize Handlers
         AdminHandler adminHandler = new AdminHandler(new AdminService(authDAO, gameDAO, userDAO));
         GameHandler gameHandler = new GameHandler(new GameService(authDAO, gameDAO));
         UserHandler userHandler = new UserHandler(new UserService(authDAO, userDAO));
@@ -28,7 +32,7 @@ public class Server {
 
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        // Register endpoints
         javalin.delete("/db", adminHandler::clear);
         javalin.post("/user", userHandler::register);
         javalin.post("/session", userHandler::login);
@@ -37,16 +41,27 @@ public class Server {
         javalin.post("/game", gameHandler::createGame);
         javalin.put("/game", gameHandler::joinGame);
 
+        // Register exception handlers
         javalin.exception(HttpResponseException.class, exceptionHandler::httpExceptionHandler);
         javalin.exception(DataAccessException.class, exceptionHandler::dataExceptionHandler);
 
     }
 
+    /**
+     * Runs the chess server on a specified port
+     * If the port given is 0, a random port is selected.
+     *
+     * @param desiredPort The port to run the server on
+     * @return The port that the server is now running on
+     */
     public int run(int desiredPort) {
         javalin.start(desiredPort);
         return javalin.port();
     }
 
+    /**
+     * Stops the server
+     */
     public void stop() {
         javalin.stop();
     }
