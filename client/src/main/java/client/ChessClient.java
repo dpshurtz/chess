506d119170627e2cd2b3b348ae47ce85c4cb8c09
Chiberty <112521861+Chiberty@users.noreply.github.com>
2026-03-24 00:22:35 -0600

    creates menu structure

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,14 +1,85 @@
 package client;
 
+import ui.UIOption;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ChessClient {
 
     ServerFacade server;
+
+    boolean isActive = true;
+    boolean isLoggedIn = false;
+
+    ArrayList<UIOption> preLoginOptions;
+    ArrayList<UIOption> postLoginOptions;
 
     public ChessClient(String serverUrl) {
         server = new ServerFacade(serverUrl);
     }
 
     public void run() {
+        Scanner scanner = new Scanner(System.in);
 
     }
+
+    private void generatePreLoginOptions() {
+        preLoginOptions = new ArrayList<>();
+        preLoginOptions.add(new UIOption("login", "description", this::login));
+        preLoginOptions.add(new UIOption("register", "description", this::register));
+        preLoginOptions.add(new UIOption("quit", "description", this::quit));
+        preLoginOptions.add(new UIOption("help", "description", this::helpPreLogin));
+    }
+
+    private void generatePostLoginOptions() {
+        postLoginOptions = new ArrayList<>();
+        postLoginOptions.add(new UIOption("play game", "description", this::playGame));
+        postLoginOptions.add(new UIOption("create game", "description", this::createGame));
+        postLoginOptions.add(new UIOption("list games", "description", this::listGames));
+        postLoginOptions.add(new UIOption("observe game", "description", this::observeGame));
+        postLoginOptions.add(new UIOption("logout", "description", this::logout));
+        postLoginOptions.add(new UIOption("help", "description", this::helpPostLogin));
+    }
+
+    private void login(){
+
+    }
+
+    private void register(){
+
+    }
+
+    private void quit(){
+
+    }
+
+    private void helpPreLogin(){
+
+    }
+
+    private void playGame(){
+
+    }
+
+    private void createGame(){
+
+    }
+
+    private void listGames(){
+
+    }
+
+    private void observeGame(){
+
+    }
+
+    private void logout(){
+
+    }
+
+    private void helpPostLogin(){
+
+    }
+
 }
---
+++ client/src/main/java/ui/UIOption.java
@@ -0,0 +1,4 @@
+package ui;
+
+public record UIOption(String name, String description, Runnable action) {
+}
